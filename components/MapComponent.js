3cd7259791af299462900220bb08f50bbf3742af
Ahmed Anwar <ahmed.anwar444442@gmail.com>
2022-04-29 14:42:44 +0200

    minor updates

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -121,7 +121,77 @@ const MapComponent = ({ children, showGPSButton = true, parkingSpaces }) => {
     <>
       <MapView
         // provider="mapbox"
-        // customMapStyle={{}}
+        customMapStyle={[
+          {
+            featureType: "all",
+            elementType: "labels.text.fill",
+            stylers: [
+              {
+                color: "#7c93a3",
+              },
+              {
+                lightness: "-10",
+              },
+            ],
+          },
+          {
+            featureType: "administrative.country",
+            elementType: "geometry",
+            stylers: [
+              {
+                visibility: "on",
+              },
+            ],
+          },
+          {
+            featureType: "administrative.country",
+            elementType: "geometry.stroke",
+            stylers: [
+              {
+                color: "#c2d1d6",
+              },
+            ],
+          },
+          {
+            featureType: "landscape",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#dde3e3",
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#c2d1d6",
+              },
+            ],
+          },
+          {
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [
+              {
+                color: "#a9b4b8",
+              },
+              {
+                lightness: "0",
+              },
+            ],
+          },
+          {
+            featureType: "water",
+            elementType: "geometry.fill",
+            stylers: [
+              {
+                color: "#a3c7df",
+              },
+            ],
+          },
+        ]}
         ref={(mapView) => {
           _mapView = mapView;
         }}
